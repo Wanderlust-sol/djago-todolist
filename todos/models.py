b5b14ac6fe1c4ab406cd15443ce32300509f7243
taylor <taylorkwon92@gmail.com>
2025-09-16 12:08:28 +0900

    fix: 모델에 verbose_name 추가

--- todos/models.py
+++ todos/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200, verbose_name="할일")
+    completed = models.BooleanField(default=False, verbose_name="완료여부")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일시")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일시")
 
     def __str__(self):
         return self.title
